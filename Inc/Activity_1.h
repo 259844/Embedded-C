56cc6f0fca56021a997e8d6ac747620d64ace336
259844 <80749311+259844@users.noreply.github.com>
2021-04-28 09:43:12 +0530

    Update Activity_1.h

--- Inc/Activity_1.h
+++ Inc/Activity_1.h
@@ -1,1 +1,20 @@
 
+/**
+ * @file Activity_1.h
+ * @author Yash Trivedi (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-27
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+
+
+
+#ifndef _port_init_h_
+#define _port_init_h_
+
+void port_init(void);
+#endif
