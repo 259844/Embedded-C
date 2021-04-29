546fd41d03a167ac56d5984fa499a6e7f8f9c95f
259844 <80749311+259844@users.noreply.github.com>
2021-04-29 09:04:57 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,30 +1,42 @@
-
+/**
+ * @file main.c
+ * @author Yash Trivedi (https://github.com/259844/Embedded-C)
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #include <avr/io.h>
-#include <util/delay.h>
 
+#include"./inc/Activity1.h"
+#include"./inc/Activity2.h"
 
 int main(void)
 {
-    DDRB |= (1<<PB1);       //output pin to led
-    DDRB &= ~(1<<PB0);    //input pin to seat button(clear bit)
-    DDRB &= ~(1<<PB2);    //input pin to heater(clear bit)
-
-
-    PORTB |= (1<<PB0);    //set bit
-    PORTB |= (1<<PB2);    //set bit
-
-    while(1)
-    {
-
-        if((!(PINB&(1<<PB0)))&(!(PINB&(1<<PB2))))
+    Activity1();
+    
+    uint16_t value = 0;
+    
+	while(1)
+    {   {
+            if  ( (!(PIND & (1<<PD2))) & (!(PIND & (1<<PD4))) )
+            {
+                PORTD |= (1<<PD3);  //TURN ON LED
+            }
+            else
+            {
+                PORTD &= ~(1<<PD3); //TURN OFF LED
+            }
+        }
+        
+        if(!(PIND&(1<<PD4))!=0)
         {
-            PORTB |= (1<<PB1);      //Turn On Led
+            value = Activity2(0);/* Read ADC channel 0 */
         }
 
-         else
-        {
-            PORTB &= ~(1<<PB1);     //Turn Off Led
-        }
     }
+
     return 0;
 }
