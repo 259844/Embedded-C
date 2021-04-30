c14e0ecd28fce7004d9172b0b5a030824b9ee8b2
259844 <80749311+259844@users.noreply.github.com>
2021-04-30 10:31:33 +0530

    Update Activity4.c

--- src/Activity4.c
+++ src/Activity4.c
@@ -8,99 +8,79 @@
  * @copyright Copyright (c) 2021
  * 
  */
-#include<avr/io.h>
-#include"../inc/Activity4.h"
 
-/**
- * @brief Initializes UART protocol parameters
- * 
- * @param ubrr_value 
- */
-void UART_init(int ubrr_value)
-{   
-    //SET BAUD RATE
-    UBRR0L = ubrr_value;
-    UBRR0H = (ubrr_value>>8)&0x00ff;
+#include "activity4.h"
 
-    UCSR0C = (1<<UMSEL00) | (1<<UCSZ01) | (1<<UCSZ00);
-
-    //ENABLE Rx AND Tx
-
-    UCSR0B = (1<<RXEN0) | (1<<TXEN0) | (1<<RXCIE0)  | (1<<TXCIE0);
-
+void USARTInit()
+{
+    UBRR0H = (uint8_t)(UBRR_VALUE>>8);
+    UBRR0L = (uint8_t)UBRR_VALUE;
+    UCSR0C=(1<<UMSEL00)|(1<<UCSZ01)|(1<<UCSZ00);
+    UCSR0B=(1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0)|(1<<TXCIE0);
 }
 
-/**
- * @brief Write function to send the temperature from micro-controller to external
- * 
- * @param data 
- */
-void UART_WRITE(int data)
+int USARTRead()
 {
+    while(!(UCSR0A &(1<<RXC0)))
+    {
+        //Do nothing
+    }
+    return UDR0;
+}
+
+void activity4_USARTWrite(uint16_t temp)
+{ 
     int i=0;
-    if((data>=0)&(data<=200))
-    {   
-        char data1[] = "20 degree celsius\n";
-        i=0;
-        while(data1[i]!='\0')
-        {
-            while(!(UCSR0A & (1<<UDRE0)));
-            UDR0 = data1[i];
+        if(temp>=0 && temp<=200){
+            unsigned char data1[]="Temperature: 20 degree C\n";
+            i=0;
+            while(data1[i]!=0){
+            while (!( UCSR0A & (1<<UDRE0)));  // Wait for empty transmit buffer
+            UDR0 = data1[i];        // Put data into buffer, sends the data
             i++;
-                            
+            }
+            _delay_ms(2000);
         }
-    }
-
-    else if((data>=210)&(data<=500))
-    {
-        char data2[] = "25 degree celsius\n";
-        i=0;
-        while(data2[i]!='\0')
-        {
-            while(!(UCSR0A & (1<<UDRE0)));
-            UDR0 = data2[i];
+        else if(temp>=210 && temp<=500){
+            unsigned char data2[]="Temperature: 25 degree C\n";
+            i=0;
+            while(data2[i]!=0){
+            while (!( UCSR0A & (1<<UDRE0)));  // Wait for empty transmit buffer
+            UDR0 = data2[i];        // Put data into buffer, sends the data
             i++;
-                            
+            }
+            _delay_ms(2000);
         }
-    }
-
-    else if((data>=510)&(data<=700))
-    {
-        char data3[] = "29 degree celsius\n";
-        i=0;
-        while(data3[i]!='\0')
-        {
-            while(!(UCSR0A & (1<<UDRE0)));
-            UDR0 = data3[i];
+        else if(temp>=510 && temp<=700){        
+             unsigned char data3[]="Temperature: 29 degree C\n";
+            i=0;
+            while(data3[i]!=0){
+            while (!( UCSR0A & (1<<UDRE0)));  // Wait for empty transmit buffer
+            UDR0 = data3[i];        // Put data into buffer, sends the data
             i++;
-                            
+            }
+            _delay_ms(2000);
         }
-    }
-
-    else if((data>=710)&(data<=1024))
-    {
-        char data4[] = "33 degree celsius\n";
-        i=0;
-        while(data4[i]!='\0')
-        {
-            while(!(UCSR0A & (1<<UDRE0)));
-            UDR0 = data4[i];
+        else if(temp>=710 && temp<=1024){         
+            unsigned char data4[]="Temperature: 33 degree C\n";     
+            i=0;
+            while(data4[i]!=0){
+            while (!( UCSR0A & (1<<UDRE0)));  // Wait for empty transmit buffer
+            UDR0 = data4[i];        // Put data into buffer, sends the data
             i++;
-                            
+            }
+            _delay_ms(2000);
         }
-    }
-
+        else{
+              unsigned char data5[]="Temperature: 0 degree C\n";
+            i=0;
+            while(data5[i]!=0){
+            while (!( UCSR0A & (1<<UDRE0)));  // Wait for empty transmit buffer
+            UDR0 = data5[i];        // Put data into buffer, sends the data
+            i++;
+            }
+            _delay_ms(2000);
+            
+        }
 }
 
-/**
- * @brief Activity-4 To send temperature according to the data sensed by ADC and generated PWM
- * 
- * @param uart_data 
- */
-void Activity4(int uart_data)
-{
-    UART_init(103);
-
-    UART_WRITE(uart_data);
-
-}
