e19ee053e6cfd5e612971ec2f65272a5621ba362
259844 <80749311+259844@users.noreply.github.com>
2021-04-30 09:01:18 +0530

    Update Activity2.c

--- src/Activity2.c
+++ src/Activity2.c
@@ -1,6 +1,10 @@
 #include<avr/io.h>
 #include"../inc/Activity2.h"
 
+/**
+ * @brief Initializes ADC Channels and its Registers
+ * 
+ */
 void ADC_init()
 {
     //    DDRA = 0x0;			/* Make ADC port as input */
@@ -8,6 +12,12 @@ void ADC_init()
 	ADMUX = 0x40;			/* Vref: Avcc, ADC channel: 0 */
 }
 
+/**
+ * @brief Activity-2 To take data from temperature sensor through ADC channel
+ * 
+ * @param channel 
+ * @return int 
+ */
 int Activity2(char channel)
 {
 	uint8_t Ain_High=0, Ain_Low=0;
@@ -22,9 +32,9 @@ int Activity2(char channel)
 
 	//_delay_us(10);
 	Ain_Low = (int)ADCL;		/* Read lower byte*/
-	Ain_High = (int)ADCH*256;		/* Read higher 2 bits and
+	Ain_High = (int)ADCH;		/* Read higher 2 bits and
 					Multiply with weight */
-	Ain = Ain_High + Ain_Low;
+	Ain = (Ain_High*256) + Ain_Low;
 	return(Ain);			/* Return digital value*/
 }
 
