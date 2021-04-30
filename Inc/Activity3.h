3267da2da03b1c6348fd74775c92f5e479419275
259844 <80749311+259844@users.noreply.github.com>
2021-04-30 10:26:34 +0530

    Update Activity3.h

--- Inc/Activity3.h
+++ Inc/Activity3.h
@@ -8,19 +8,48 @@
  * @copyright Copyright (c) 2021
  * 
  */
-#ifndef _Activity3_h_
-#define _Activity3_h_
-
+#ifndef __ACTIVITY_3_H__
+#define __ACTIVITY_3_H__
 /**
- * @brief Initializes Timer0(8-bit) 
+ * @file activity3.h
+ * @author KaaviyaP (kaaviya2881999@gmail.com)
+ * @brief Header file for activity3
+ * @version 0.1
+ * @date 2021-04-27
+ * 
+ * @copyright Copyright (c) 2021
  * 
  */
-void Timer_init();
 
 /**
- * @brief Activity-3 To generate PWM according to the data sensed by the ADC channel
+ * Macro Definitions
+ */
+#define PWM_20_PERCENT (205) /**< Output PWM for 20% duty cycle */
+#define PWM_40_PERCENT (410) /**< Output PWM for 40% duty cycle */
+#define PWM_70_PERCENT (717) /**< Output PWM for 70% duty cycle */
+#define PWM_95_PERCENT (973) /**< Output PWM for 95% duty cycle */
+
+/**
+ * Include files
+ */ 
+#include <util/delay.h>
+#include <avr/io.h>
+
+/**
+ * Function Definitions
+ */
+
+/**
+ * @brief Initialize Timer1 registers 
  * 
  */
-void Activity3(int);
+void InitTimer();
 
-#endif
+/**
+ * @brief Produce duty cycle in PWM according to i/p ADC value
+ * 
+ * @param[in] temp The ADC value from activity2
+ */
+void activity3_PWM(uint16_t temp);
+
+#endif /* __ACTIVITY_3_H__ */
