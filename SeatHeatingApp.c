b2a509c1eb91a3f50363508ce32e2fad1996ffc7
259844 <80749311+259844@users.noreply.github.com>
2021-05-01 08:31:16 +0530

    Update SeatHeatingApp.c

--- SeatHeatingApp.c
+++ SeatHeatingApp.c
@@ -8,21 +8,23 @@
  * @copyright Copyright (c) 2021
  * 
  */
-#include <avr/io.h>
-#include <util/delay.h>
-#include<Activity1.h>
-#include<Activity2.h>
-#include<Activity3.h>
+#include<avr/io.h>
+#include"./inc/Activity1.h"
+#include"./inc/Activity2.h"
+#include"./inc/Activity3.h"
+#include"./inc/Activity4.h"
 
 
-
-
-int main(void)
-{
+int main()
+{   int read_adc = 0,timer_val=0;
     while(1)
     {
-        /*Activity 1,2,4 are called in Activity 3*/
-        Activity3();
+        Activity1();
+        if((!(PINB&(1<<PB0)))&(!(PINB&(1<<PB2))))
+        {
+            read_adc = Activity2();
+            timer_val = Activity3(read_adc);
+            Activity4(timer_val);
+        }
     }
-    return 0;
-}
+return 0;
